be9f8cc7c407c3260961f7b02a8a49838250b521
HuzaifaAbdulRehman <huzaifarehman897@gmail.com>
2025-10-07 01:05:43 +0500

    Remove client static file serving from production backend

--- server/index.js
+++ server/index.js
@@ -59,14 +59,6 @@ app.get('/api/health', (req, res) => {
   });
 });
 
-// Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}
-
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
